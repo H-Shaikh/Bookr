b5fafeca0b3f30e6efe1c2f7498309721007c543
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2023-11-17 18:42:38 +0530

    search-template

--- bookr/urls.py
+++ bookr/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from reviews.views import index
+from reviews.views import index, book_search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('book-search/', book_search)
 ]
