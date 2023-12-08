7bae216aae10563f54e5a3c4d9392292a9ba0b51
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2023-12-08 16:25:30 +0530

    function based view

--- bookr/urls.py
+++ bookr/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from reviews.views import index, book_search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('book-search/', book_search)
+    # path('', index),
+    # path('book-search/', book_search)
+    path('', include('reviews.urls'))
 ]
