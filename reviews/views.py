16d0be80f8b80d9b7cd75da70d0f40816ccb9601
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2024-01-01 10:00:46 +0530

    templates

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from .models import Book
+from .utils import average_rating
 
 
 def index(request):
@@ -12,6 +13,22 @@ def book_search(request):
     return render(request, "search-results.html", {"search_text": search_text})
 
 
-def welcome_view(request):
-    message = f'<html><h1>Welcome to Bookr!</h1><p>{Book.objects.count()} books and counting!</p></html>'
-    return HttpResponse(message)
+def book_list(request):
+    books = Book.objects.all()
+    books_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        books_list.append({'book': book,
+                           'book_rating': book_rating,
+                           'number_of_reviews': number_of_reviews})
+
+    context = {
+        'books_list': books_list
+    }
+    return render(request, 'reviews/book_list.html', context)
